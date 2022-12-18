488e255af5d570cc516c7d5ae35b74967c6e3774
superryanguo <superryanguo@outlook.com>
2022-12-18 17:22:32 +0800

    can workable version

--- model/init/init.sql
+++ model/init/init.sql
@@ -24,10 +24,10 @@ insert into category (category_id, category_name)
 values(null, "充电宝");
 
 insert into `product`(`id`,`name`,`category_id`,`title`,`info`,`img_path`,`price`,`discount_price`,`product_num`,`product_sales`) values
-(1,'小米CC9 Pro',1,'1亿像素,五摄四闪','1亿像素主摄 / 全场景五摄像头 / 四闪光灯 / 3200万自拍 / 10 倍混合光学变焦，50倍数字变焦 / 5260mAh ⼤电量 / 标配 30W疾速快充 / ⼩米⾸款超薄屏下指纹 / 德国莱茵低蓝光认证 / 多功能NFC / 红外万能遥控 / 1216超线性扬声器','public/imgs/phone/Mi-CC9.png',2799,2599,20,0);
+(1,'小米CC9 Pro',1,'1亿像素,五摄四闪','1亿像素主摄 / 全场景五摄像头 / 四闪光灯 / 3200万自拍 / 10 倍混合光学变焦，50倍数字变焦 / 5260mAh ⼤电量 / 标配 30W疾速快充 / ⼩米⾸款超薄屏下指纹 / 德国莱茵低蓝光认证 / 多功能NFC / 红外万能遥控 / 1216超线性扬声器','http://localhost:3000/public/imgs/phone/Mi-CC9.png',2799,2599,20,0);
 
 insert into `product_img`(`id`,`product_id`,`img_path`,`intro`) values
-(1,1,'public/imgs/phone/picture/Redmi-k30-1.png',null),
-(110,35,'public/imgs/phone/picture/protectingShell-Mix-3-1.jpg',null);
+(1,1,'http://localhost:3000/public/imgs/phone/picture/Redmi-k30-1.png',null),
+(110,35,'http://localhost:3000/public/imgs/phone/picture/protectingShell-Mix-3-1.jpg',null);
 
 SET FOREIGN_KEY_CHECKS = 1;
